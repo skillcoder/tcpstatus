d58e656b9739671eb6b15e4bec051b5165d9b7fb
skillcoder <mail@skillcoder.com>
2018-10-09 00:20:56 +0300

    Add connection loop

--- main.go
+++ main.go
@@ -5,13 +5,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"net"
 	"os"
 	"os/exec"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
-	"regexp"
 
 	"github.com/sirupsen/logrus"
 )
@@ -40,37 +41,49 @@ func AlarmConnection() {
 		say = "connection_lost.mp3"
 	}
 
-	cmd := exec.Command("mplayer", say)
-	log.Infof("mplayer %s", say)
-	err := cmd.Run()
-	if err != nil {
-		log.Errorf("Error in run player: $v", err)
-	}
+	playSound(say)
 
 	os.Exit(2)
 }
 
-func main() {
-	servAddr := os.Getenv("TCPING_ADDR")
-	if len(servAddr) == 0 {
-		log.Fatal("Required env parameter TCPING_ADDR [host:port] is not set")
+func playSound(name string) {
+	cmd := exec.Command("mplayer", name)
+	log.Infof("mplayer %s", name)
+	err := cmd.Run()
+	if err != nil {
+		log.Errorf("Error in run player: $v", err)
+	}
+}
+
+func waitConnection(tcpAddr *net.TCPAddr) (conn *net.TCPConn) {
+	var delay = 200
+	var factor = 2.0
+	var maxsleep = 5000
+	var err error
+	for {
+		conn, err = net.DialTCP("tcp", nil, tcpAddr)
+		if err != nil {
+			log.Warnf("Dial failed: %s", err.Error())
+			time.Sleep(time.Duration(delay) * time.Millisecond)
+			delay = int(math.Ceil(float64(delay) * factor))
+			if delay > maxsleep {
+				delay = maxsleep
+			}
+		} else {
+			playSound("connected.mp3")
+			break
+		}
 	}
 
+	return conn
+}
+
+func monitor(tcpAddr *net.TCPAddr) {
 	timeoutDuration := 2500 * time.Millisecond
 	slowDetectionMS := 138
 	lagDetectionMS := 400
 
-	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
-	if err != nil {
-		log.Info("ResolveTCPAddr failed:", err.Error())
-		os.Exit(1)
-	}
-
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	if err != nil {
-		log.Info("Dial failed:", err.Error())
-		os.Exit(1)
-	}
+	conn := waitConnection(tcpAddr)
 
 	defer func() {
 		log.Info("Disconnect")
@@ -83,7 +96,7 @@ func main() {
 	var ts int64
 	for {
 		ts = makeTimestamp()
-		_, err = fmt.Fprintf(bufWriter, "%d\n", ts)
+		_, err := fmt.Fprintf(bufWriter, "%d\n", ts)
 		if err != nil {
 			log.Info("Write to server failed:", err.Error())
 			reason = err.Error()
@@ -117,5 +130,22 @@ func main() {
 		}
 
 		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func main() {
+	servAddr := os.Getenv("TCPING_ADDR")
+	if len(servAddr) == 0 {
+		log.Fatal("Required env parameter TCPING_ADDR [host:port] is not set")
+	}
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
+	if err != nil {
+		log.Info("ResolveTCPAddr failed:", err.Error())
+		os.Exit(1)
+	}
+
+	for {
+		monitor(tcpAddr)
 	}
 }
